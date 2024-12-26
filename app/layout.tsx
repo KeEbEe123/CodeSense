8fc9771ed7d2b6d05faa74d2ed091b8b7f53e2b2
KeEbEe123 <keertan.k@gmail.com>
2024-12-26 14:36:09 +0530

    navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Koulen } from "next/font/google";
+
+const koulen = Koulen({
+  weight: "400",
+  variable: "--font-koulen",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${koulen.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
 "use client";
-import { FormEvent, useState } from "react";
-import { useRouter } from "next/navigation";
-export default function Home() {
-  const [inputVal, setInputVal] = useState("");
-  const { push } = useRouter();
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    push(`/profile/${inputVal}`);
-  };
+
+import React from "react";
+import Navbar from "@/components/Navbar";
+function Home() {
   return (
-    <div className="flex justify-center flex-col items-center">
-      <div>
-        <h1>Enter name: </h1>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="text-black"
-          type="text"
-          placeholder="type your name"
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-        />
-        <button type="submit">predict data</button>
-      </form>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
+
+export default Home;
