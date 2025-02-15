965954eb82b8602bf3f36d5fecca258d619fab41
Siddharth <siddhartht4206@gmail.com>
2025-02-15 19:07:21 +0530

    Contact Page Update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,6 @@ const page = () => {
     <div className="flex flex-col justify-center items-center h-screen font-koulen text-cyan-700">
       <h1>Contact us @ codesense753@gmail.com if you face any issues</h1>
       <p className="text-red-700">
-        Hi frends I am sidsmartz._.ig 5'11 listens to lana del ray yay
       </p>
     </div>
   );
