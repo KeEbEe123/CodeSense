0fd337745ba92c2a867ad68e63475ffcffd3c944
KeEbEe123 <keertan.k@gmail.com>
2025-02-25 12:11:01 +0530

    changed courses api again

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "@/app/auth/[...nextauth]";
 import connectMongoDB from "@/lib/mongodb";
 import Course from "@/models/Course";
 
