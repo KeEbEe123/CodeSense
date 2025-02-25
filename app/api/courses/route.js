52980399af5ee0a45fbd9f52ef0dd246fd513bed
KeEbEe123 <keertan.k@gmail.com>
2025-02-25 12:17:58 +0530

    fixed import in courses api

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/auth/[...nextauth]";
+import { authOptions } from "../auth/[...nextauth]/route";
 import connectMongoDB from "@/lib/mongodb";
 import Course from "@/models/Course";
 
