eda0673a41aa74089f41b2a26676e55151734dc5
KeEbEe123 <keertan.k@gmail.com>
2025-03-03 22:10:30 +0530

    reverting protection

--- app/api/update-codechef-stats/route.js
+++ app/api/update-codechef-stats/route.js
@@ -4,41 +4,7 @@ import User from "../../../models/user";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 
-const ADMIN_EMAILS = process.env.NEXT_PUBLIC_ADMIN_EMAILS?.split(",") || [];
-
 export async function POST(request) {
-  const session = await getServerSession(authOptions);
-  let ip =
-    request.headers.get("x-forwarded-for") ||
-    request.headers.get("x-real-ip") ||
-    request.ip ||
-    "Unknown IP";
-
-  // If multiple IPs exist, get the first one
-  if (ip.includes(",")) {
-    ip = ip.split(",")[0].trim();
-  }
-
-  console.log(`Incoming request from IP: ${ip}`);
-
-  // Authentication and authorization checks
-  if (!session || !session.user) {
-    console.warn(`Unauthorized access attempt from IP: ${ip}`);
-    return NextResponse.json(
-      { message: "Unauthorized: You must be signed in" },
-      { status: 401 }
-    );
-  }
-
-  if (!ADMIN_EMAILS.includes(session.user.email)) {
-    console.warn(
-      `Forbidden access attempt by ${session.user.email} from IP: ${ip}`
-    );
-    return NextResponse.json(
-      { message: "Forbidden: You don't have permission to add courses" },
-      { status: 403 }
-    );
-  }
   try {
     const { email, codechefUsername, stats } = await request.json();
 
